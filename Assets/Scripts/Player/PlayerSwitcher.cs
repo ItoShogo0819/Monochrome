b3e4abeb32d01ed90d7047bfec0cc02ba70ebe30
flaming8634 <flaming.8634@icloud.com>
2026-01-12 00:29:01 +0900

    [Update]審査会前の調整

--- Assets/Scripts/Player/PlayerSwitcher.cs
+++ Assets/Scripts/Player/PlayerSwitcher.cs
@@ -15,19 +15,19 @@ public class PlayerSwitcher : MonoBehaviour
     {
         if (MainCamera.activeSelf)          // メインカメラがアクティブ
         {
-            BlackPlayerMove.IsControllable = true;   // 黒操作可
-            WhitePlayerMove.IsControllable = false;  // 白操作不可
+            BlackPlayerMove.IsControllable = true;   // 黒は操作可
+            WhitePlayerMove.IsControllable = false;  // 白は操作不可
 
-            BlackPlayerJump.JumpControl = true;      // 黒ジャンプ可
-            WhitePlayerJump.JumpControl = false;    // 白ジャンプ不可
+            BlackPlayerJump.JumpControl = true;      // 黒はジャンプ可
+            WhitePlayerJump.JumpControl = false;    // 白はジャンプ不可
         }
         else if (SabCamera.activeSelf)      // サブカメラがアクティブ
         {
-            BlackPlayerMove.IsControllable = false;  // 黒操作不可
-            WhitePlayerMove.IsControllable = true;   // 白操作可
+            BlackPlayerMove.IsControllable = false;  // 黒は操作不可
+            WhitePlayerMove.IsControllable = true;   // 白は操作可
 
-            BlackPlayerJump.JumpControl = false;     // 黒ジャンプ不可
-            WhitePlayerJump.JumpControl = true;      // 白ジャンプ可
+            BlackPlayerJump.JumpControl = false;     // 黒はジャンプ不可
+            WhitePlayerJump.JumpControl = true;      // 白はジャンプ可
         }
     }
 }
